6bd67110750566de7113db44a7c410ab069a4823
saddamhosan <contact.saddamhosan.com>
2022-04-26 21:07:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { request } = require('express');
+const app=express()
+const port=process.env.PORT||5000
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xoizd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+    try {
+        await client.connect();
+        const eventCollection = client.db("volunteer-work").collection("events");
+   app.get('/events',async(req,res)=>{
+    const query={}
+    const cursor=eventCollection.find(query)
+    const result=await cursor.toArray()
+    res.send(result);
+   }) 
+   
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send('welcome to volunteer work')
+})
+
+app.listen(port,()=>{
+    console.log('listening to port',port);
+})
